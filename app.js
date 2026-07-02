273cab26d2ab32dfe599b2d7eac8c6c55b2956d5
Anusha <root@ip-172-31-7-220.ap-south-1.compute.internal>
2026-07-02 06:41:08 +0000

    Second commit.Version 3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: "CI/CD Pipeline Working.Welcome to v2 🚀" });
+  res.json({ message: "CI/CD Pipeline Working.Welcome to v3 🚀" });
 });
 
 app.get('/health', (req, res) => {
